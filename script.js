136f3229bb52a597247fefbb45f631c53af9858f
Raihan Adliputra <raihan@crescentrating.com>
2024-05-21 22:58:49 +0700

    finalize desktop mode

--- script.js
+++ script.js
@@ -1,45 +1,24 @@
-let menuIcon = document.querySelector('#navbar-icon');
-let navbar = document.querySelector('.navbar');
-let sections = document.querySelectorAll('section');
-let navLinks = document.querySelectorAll('header nav a');
-
-window.onscroll = () => {
-    sections.forEach(section => {
-        let top = window.scrollY;
-        let offset = section.offsetTop - 150;
-        let height = section.offsetHeight;
-        let id = section.getAttribute('id');
-
-        if (top >= offset && top < offset + height) {
-            navLinks.forEach(links => {
-                links.classList.remove('active');
-                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
-            });
-        }
-    });
-};
-
-menuIcon.onclick = () => {
-    menuIcon.classList.toggle('bx-x');
-    navbar.classList.toggle('active');
-};
-
-window.addEventListener('scroll', () => {
-    const header = document.querySelector('.header');
-    const contact = document.getElementById('contact');
-
-    const contactEnd = contact.offsetTop + contact.offsetHeight;
-
-    // Check if the section is at the top of the viewport
-    if (window.scrollY >= contact.offsetTop && window.scrollY <= contactEnd) {
-        header.classList.add('color-for-contact');
-    } else {
-        header.classList.remove('color-for-contact');
-    }
-});
-
 document.addEventListener('DOMContentLoaded', function() {
-    document.querySelector('.contact-button').addEventListener('click', function() {
-        window.location.href = '#contact';
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('.navbar a');
+
+    window.addEventListener('scroll', () => {
+        let current = '';
+
+        sections.forEach(section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+
+            if (pageYOffset >= (sectionTop - sectionHeight / 3)) {
+                current = section.getAttribute('id');
+            }
+        });
+
+        navLinks.forEach(link => {
+            link.classList.remove('active');
+            if (link.getAttribute('href').includes(current)) {
+                link.classList.add('active');
+            }
+        });
     });
 });
